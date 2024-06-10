6d5cd5a36f5b841d4b51bb77452245294d8002df
nanditha16 Murthy <nanditha.murthy@gmail.com>
2024-06-10 01:35:11 +0000

    updated powershell script bug fixes

--- s3/powershell_scripts/New-S3Bucket.ps1
+++ s3/powershell_scripts/New-S3Bucket.ps1
@@ -1,30 +1,30 @@
 Import-Module AWS.Tools.S3
 
 $region = "us-east-1"
-
 $bucketName = Read-Host -Prompt 'Enter the S3 bucket name'
 
-Write-Host "AWS- Region: $region"
+Write-Host "AWS Region: $region"
 Write-Host "S3 Bucket: $bucketName"
 
-
+# Function to check if the bucket exists
 function BucketExists {
-    $bucket = GET-S3Bucket -BucketName $bucketName -ErrorAction SilentlyContinue
+    param (
+        [string]$bucketName
+    )
+    $bucket = Get-S3Bucket -BucketName $bucketName -ErrorAction SilentlyContinue
     return $null -ne $bucket
 }
 
-if (-not (BucketExists)){
-    Write-Host "S3 Bucket: $bucketName doesnt exists"
+if (-not (BucketExists -bucketName $bucketName)){
+    Write-Host "S3 Bucket: $bucketName doesn't exist"
     New-S3Bucket -BucketName $bucketName -Region $region
 } else {
     Write-Host "S3 Bucket: $bucketName already exists"
-    #create a new file
+    # Create a new file
     $fileName = 'myfile.txt'
     $fileContent = 'Hello World!'
     Set-Content -Path $fileName -Value $fileContent
 
+    # Upload the file to S3 bucket
     Write-S3Object -BucketName $bucketName -File $fileName -Key $fileName
-
 }
-
-
